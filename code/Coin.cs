ee87f08a47f754c667c0f75a4219f92e2a43d692
ElaiH <56837213+ElaiH@users.noreply.github.com>
2024-03-29 17:52:58 +0300

    coinroatate kooler

--- code/Coin.cs
+++ code/Coin.cs
@@ -3,6 +3,9 @@ using Sandbox;
 public sealed class Coin : Component, Component.ITriggerListener
 {
 	[Property] CoinManager coinMaster;
+	[Property] bool roatatearound;
+	[Property] bool roatateup;
+	[Property] bool roatatePitch;
 	void ITriggerListener.OnTriggerEnter( Collider other )
 	{
 		Log.Info( "yipeeee" );
@@ -17,6 +20,20 @@ public sealed class Coin : Component, Component.ITriggerListener
 	protected override void OnUpdate()
 	{
 		base.OnUpdate();
-		Transform.Rotation *= Rotation.FromYaw(Time.Delta * 100);
+
+		if( roatateup )
+		{
+			Transform.Rotation *= Rotation.FromRoll( Time.Delta * 100 );
+		}
+
+		if ( roatatearound )
+		{
+			Transform.Rotation *= Rotation.FromYaw( Time.Delta * 100 );
+		}
+
+		if(roatatePitch )
+		{
+			Transform.Rotation *= Rotation.FromPitch( Time.Delta * 100 );
+		}
 	}
 }
